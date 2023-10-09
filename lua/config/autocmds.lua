c769c3aa8055fa1d78e50021ddd5eb0e855b196c
rustysec <russ@infocyte.com>
2023-10-09 06:19:51 -0700

    keymap and autocmd updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,15 +4,23 @@
 
 vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = "LspAttach_inlayhints",
-  callback = function(args)
-    if not (args.data and args.data.client_id) then
-      return
-    end
+    group = "LspAttach_inlayhints",
+    callback = function(args)
+        if not (args.data and args.data.client_id) then
+            return
+        end
 
-    local bufnr = args.buf
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    require("lsp-inlayhints").on_attach(client, bufnr, true)
-  end,
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        require("lsp-inlayhints").on_attach(client, bufnr, true)
+    end,
 })
 vim.cmd("hi! link LspInlayHint Comment")
+
+vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = "YankHighlight",
+    desc = "Highlight yanked text",
+    pattern = "*",
+    command = "silent! lua vim.highlight.on_yank()",
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.del("n", "<leader>gg")
+vim.keymap.del("n", "<leader>gG")
+
 vim.keymap.set("n", "<leader>e", require("oil").open, { desc = "Open parent directory" })
-vim.keymap.set("n", "<leader><leader>", ": w<CR>", { desc = "Save" })
-vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Blame" })
+vim.keymap.set("n", "<leader><leader>", ": w<cr>", { desc = "Save" })
+-- vim.keymap.set("n", "<leader>gb", ":Git blame<cr>", { desc = "Blame" })
+vim.keymap.set("n", "<leader>gd", ":Telescope diagnostics<cr>", { desc = "Diagnostics" })
+vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<cr>", { desc = "Git reset hunk" })
+vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<cr>", { desc = "Git preview hunk inline" })
+vim.keymap.set("n", "<leader>gP", ":Gitsigns preview_hunk<cr>", { desc = "Git preview hunk" })
+vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<cr>", { desc = "Git next hunk" })
+vim.keymap.set("n", "<leader>gl", ":Gitsigns prev_hunk<cr>", { desc = "Git previous hunk" })
+vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<cr>", { desc = "Git blame line" })
